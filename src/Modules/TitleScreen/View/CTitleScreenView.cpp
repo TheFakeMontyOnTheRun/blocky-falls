78cd9d5dcb0cbd1adc001860b584dbc3de19bdf1
= <danielmonteiro@id.uff.br>
2016-06-08 23:39:24 -0300

    A very simple animation on the title screen
    Just because.

--- src/Modules/TitleScreen/View/CTitleScreenView.cpp
+++ src/Modules/TitleScreen/View/CTitleScreenView.cpp
@@ -1,4 +1,8 @@
 #include "Vipper/Vipper.h"
+#include "Vipper/CLerp.h"
+
+#include "Modules/Gameplay/Entities/CColumn.h"
+
 #include "Modules/TitleScreen/View/CRect.h"
 #include "Modules/TitleScreen/View/CPlayButton.h"
 #include "Modules/TitleScreen/View/CTitleScreenView.h"
@@ -6,12 +10,18 @@
 
 namespace BlockyFalls {
 	
+	Vipper::CLerp lerp(0 , 200, 2000);
+	long ellapsed = 0;
 	CTitleScreenView::CTitleScreenView( std::shared_ptr<Vipper::IRenderer> renderer ) : IView( renderer ) {
 		mButton.mBounds = CRect( 200, 300, 250, 350 );
 	}
 	
 	void CTitleScreenView::show() {
-		getRenderer()->drawSquare( mButton.mBounds.mX0, mButton.mBounds.mY0, mButton.mBounds.mX1, mButton.mBounds.mY1, 0xFF0000 );	
+		auto renderer = getRenderer();
+		renderer->drawSquare( 0,0, 640, 480, 0 );
+		ellapsed += 33;
+		long delta = lerp.getValue( ellapsed );
+		renderer->drawSquare( mButton.mBounds.mX0 + delta, mButton.mBounds.mY0, mButton.mBounds.mX1 + delta, mButton.mBounds.mY1, 0xFF0000 );	
 	}
 	
 	void CTitleScreenView::onClick( std::pair<int, int> position ) {
